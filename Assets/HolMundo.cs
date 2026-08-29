0605b670ca852a94eae2ae76fc56f575b94990ef
valeryarg <baleriarg98@gmail.com>
2026-08-29 14:17:31 -0600

    Hola mundo Valeria

---
+++ Assets/HolMundo.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HolMundo : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+         Debug.Log("Hola mundo");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
